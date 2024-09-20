2c4ed43a9b89ab783170df3f83ca492a66f3a25d
unknown <syeddh01@gmail.com>
2024-09-20 08:05:38 +0530

    project done

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const result = document.querySelector('#results');
+    const bmiGuide = document.querySelector('#bmi');
+
+        if(height === '' || height < 0 || isNaN(height)){
+            result.innerHTML = `Please give a valid height`
+        }else if(weight === '' || weight < 0 || isNaN(weight)){
+            result.innerHTML = `Please give a valid weight`
+        }else{
+            const bmi = (weight / ((height *height) / 10000)).toFixed(2);
+            result.innerHTML = `<span>Your BMI is ${bmi}</span>`;
+
+            if(bmi < 18.6 ){
+                bmiGuide.innerHTML = `You are underweight`
+            }else if(bmi >= 18.6 && bmi <= 24.9){
+                bmiGuide.innerHTML = `You are healthy`
+            }else if(bmi >= 24.9){
+                bmiGuide.innerHTML = `You are overweight`
+            }
+        }
+
+    
+})
